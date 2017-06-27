13abb13af5386e6eded889769259c212e689cedf
Moumita Patra <patramoumita9@gmail.com>
2017-06-27 18:30:29 -0500

    modified TwoSum

--- custom/src/com/java/dev/TwoSum.java
+++ custom/src/com/java/dev/TwoSum.java
@@ -6,12 +6,28 @@ import java.util.Map;
 public class TwoSum {
 
 	public static void main(String[] args) {
-		int num[] = {3,2,0,6};
-    	int target = 6;
+		int num[] = {3,2,5,1};
+    	int target = 7;
     	int arry[] = twoSum(num, target);
     	for(int n : arry)
     		System.out.println(n);
 	}
+	
+	//another way
+//	public static int[] twoSum(int[] nums, int target) {
+//        int arr[] = new int[2];
+//        for(int i=0; i<nums.length; i++){
+//        	for(int j = i+1; j<nums.length; j++) {
+//            if(nums[i]+nums[j]==target) {
+//                arr[0] = i;
+//                arr[1] = j;
+//                return arr;
+//            }
+//        	}
+//        }
+//        throw new IllegalArgumentException("No two sum solution");
+//    }
+	
 	public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
