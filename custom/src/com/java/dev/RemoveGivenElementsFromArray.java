d34e2a61e59643d40022b9a5773ad27a54ddfdcd
Moumita Patra <patramoumita9@gmail.com>
2017-07-03 01:39:43 -0500

    code pushed

--- custom/src/com/java/dev/RemoveGivenElementsFromArray.java
+++ custom/src/com/java/dev/RemoveGivenElementsFromArray.java
@@ -7,7 +7,18 @@ public class RemoveGivenElementsFromArray {
 	public static void main(String[] args) {
 		int nums[]={2,2,3};
 		int val=2;
+		System.out.println(removeElements(nums, val));
 		System.out.println(removeElement(nums, val));
+	}
+	private static int removeElements(int[] nums, int val) {
+		 int i=0;
+			for(int j=0; j<nums.length; j++) {
+				if(nums[j]!=val){
+	                nums[i]=nums[j];
+					i++;
+	        }
+			}
+			return i;
 	}
 	public static int removeElement(int[] nums, int val) {
 		int count=0;
