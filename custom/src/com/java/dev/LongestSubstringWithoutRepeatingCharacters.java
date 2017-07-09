861d37e0714be76d56cca2752e112dd4952aa921
Moumita Patra <patramoumita9@gmail.com>
2017-07-08 23:36:30 -0500

    code pushed.

--- custom/src/com/java/dev/LongestSubstringWithoutRepeatingCharacters.java
+++ custom/src/com/java/dev/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,28 +1,32 @@
 package com.java.dev;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters {
 
 	public static void main(String[] args) {
-		String str = "abccdefg";
+		String str = "abcbbcdd";
 		System.out.println(lengthOfLongestSubstring(str));
 	}
 	
-	 public static int lengthOfLongestSubstring(String str) {
-	        Set<Character> setOfChars = new HashSet<>();
-			int count=0, j=0;
-			for(int i=0; i< str.length(); i++) {
-				if(!setOfChars.contains(str.charAt(i))) {
-				setOfChars.add(str.charAt(i));
-				count = Math.max(count, i - j);
-				}
-				else {
-					setOfChars.remove(str.charAt(j++));
+	 public static int lengthOfLongestSubstring(String s) {
+		 int n = s.length();
+	        Set<Character> set = new HashSet<>();
+	        int ans = 0, i = 0, j = 0;
+	        while (i < n && j < n) {
+	            // try to extend the range [i, j]
+	            if (!set.contains(s.charAt(j))){
+	                set.add(s.charAt(j++));
+	                ans = Math.max(ans, j - i);
 	            }
-			}
-			return count;
+	            else {
+	                set.remove(s.charAt(i++));
+	            }
+	        }
+	        return ans;
 	    }
 
 }
